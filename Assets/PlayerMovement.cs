a32b9117c338d73a79a471282156299b1f859ce0
Foxd5 <38409373+Foxd5@users.noreply.github.com>
2024-08-17 20:50:46 -0400

    Movement Constraints
    added constraints so player cant move off screen

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
     public float speed = 10.5f;
+
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the screen bounds based on the camera's view
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        // Get the width and height of the object (the ship)
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; // Half the width
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; // Half the height
+    }
 
     // Update is called once per frame
     void Update()
@@ -29,6 +43,17 @@ public class NewBehaviourScript : MonoBehaviour
             pos.x -= speed * Time.deltaTime;
         }
 
-        transform.position = pos;
+        // Call the boundary clamp function
+        transform.position = ClampPosition(pos);
+    }
+
+    // Function to clamp the position within the screen bounds
+    private Vector3 ClampPosition(Vector3 pos)
+    {
+        // Clamp the position so the ship doesn't move off-screen
+        pos.x = Mathf.Clamp(pos.x, -screenBounds.x + objectWidth, screenBounds.x - objectWidth);
+        pos.y = Mathf.Clamp(pos.y, -screenBounds.y + objectHeight, screenBounds.y - objectHeight);
+        return pos;
     }
 }
+
