9ab70d60999e4eb746bdc9bdc9c1a4581b61aabc
Foxd5 <38409373+Foxd5@users.noreply.github.com>
2024-08-26 13:35:37 -0400

    Lives Counter
    created a lives counter. if health goes to 0 lives decreases and after 3 lives are used the scene resets.

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthManager : MonoBehaviour
 {
     public Image healthBar;
-    public float healthAmount = 100f; 
+    public float healthAmount = 100f;
+    public int MaxLives = 3;
+    private int CurrentLives;
+    public TextMeshProUGUI LivesCounterText;
 
     // Start is called before the first frame update
     void Start()
     {
+        CurrentLives = MaxLives;
+        UpdateLivesUI();
 
-        if (healthBar == null)
+
+        /* if (healthBar == null)
         {
-            Debug.LogError("Health bar Image is not assigned in the Inspector."); //debug testing !
-        }
+            Debug.LogError("Health bar Image is not assigned in the Inspector."); //debug testing!!! yay!
+        }*/
 
     }
 
@@ -26,14 +33,15 @@ public class HealthManager : MonoBehaviour
         if (healthAmount <= 0) //testing damage and healing. enter to take dam,
                                //space to heal, if at 0 level should reset. 
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);//This line just reloaded the scene. I want to lose lives until the game ends.
+            LoseLife();
         }
         if (Input.GetKeyDown(KeyCode.Return)) //enter does 20 damage to ship
         {
             TakeDamage(20);
         }
 
-        if (Input.GetKeyDown(KeyCode.B)) //space bar heals 5 damage
+        if (Input.GetKeyDown(KeyCode.B)) //B button heals 5 damage
         {
             Heal(5);
         }
@@ -52,4 +60,24 @@ public class HealthManager : MonoBehaviour
         healthAmount = Mathf.Clamp(healthAmount, 0, 100);
         healthBar.fillAmount = healthAmount / 100f;
     }
+    void LoseLife()
+    {
+        CurrentLives--;
+        UpdateLivesUI();
+
+        if(CurrentLives <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); //remember, just resets the scene.
+        }
+        else
+        {
+            healthAmount = 100f;
+            healthBar.fillAmount = healthAmount / 100f; 
+        }
+    }
+    void UpdateLivesUI()
+    {
+        LivesCounterText.text = "Lives: " + CurrentLives;
+    }
+
 }
