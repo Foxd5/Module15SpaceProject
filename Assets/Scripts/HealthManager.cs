99f6bc65a0bf590ea97ab15772e9c5b3abbe3239
Foxd5 <38409373+Foxd5@users.noreply.github.com>
2024-08-26 15:43:00 -0400

    Game over & Restart
    added gameover functionality and made a game over screen. Also made the gameover screen have a restart button that succesfully restarts the game.

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -11,13 +11,20 @@ public class HealthManager : MonoBehaviour
     public float healthAmount = 100f;
     public int MaxLives = 3;
     private int CurrentLives;
-    public TextMeshProUGUI LivesCounterText;
+
+    public GameObject miniShipPrefab;
+    public Transform ShipLivesPanel;
+    public GameObject GameOverPanel;
+
+    public TextMeshProUGUI LivesCounterText; //for displaying lives in text. but i want it in ships!
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1f; //adding this fixed the issue of the game being paused upon gameover restart.
         CurrentLives = MaxLives;
         UpdateLivesUI();
+        GameOverPanel.SetActive(false); //makes sure the gameover panel is hidden on startup
 
 
         /* if (healthBar == null)
@@ -67,7 +74,8 @@ public class HealthManager : MonoBehaviour
 
         if(CurrentLives <= 0)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name); //remember, just resets the scene.
+            GameOver();
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name); //remember, just resets the scene.
         }
         else
         {
@@ -77,7 +85,31 @@ public class HealthManager : MonoBehaviour
     }
     void UpdateLivesUI()
     {
-        LivesCounterText.text = "Lives: " + CurrentLives;
-    }
+        foreach (Transform child in ShipLivesPanel)
+        {
+            Destroy(child.gameObject);
+        }
 
+        for(int i = 0; i < CurrentLives; i++)
+        {
+            GameObject miniShip = Instantiate(miniShipPrefab, ShipLivesPanel);
+
+            RectTransform shipRect = miniShip.GetComponent<RectTransform>();
+            shipRect.anchoredPosition = new Vector2(i * 100f, 0);
+
+        }
+        LivesCounterText.text = "Lives: "; //+ CurrentLives;//this was for the text display of lives. we want prefabs though!
+    }
+    void GameOver()
+    {
+        GameOverPanel.SetActive(true);
+
+        Time.timeScale = 0f; //stops the game! 
+    }
+   public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
+    }
 }
